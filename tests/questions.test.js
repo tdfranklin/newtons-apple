d03f594d3d822c0110a169afdd2d06ccc7f38215
Steven Peniche <stevenmpeniche@gmail.com>
2018-03-15 00:44:30 -0400

    testing  helpers, questions and create-component (#12)
    testing helpers, questions and create-component

---
+++ tests/questions.test.js
@@ -0,0 +1,25 @@
+const questions = require('../bin/questions');
+
+describe('questions', () => {
+  it('holds the correct value', () => {
+    const expectedVal = [
+      {
+        type: 'checkbox',
+        message: 'Choose lifecycle methods',
+        name: 'methods',
+        choices: [
+          'componentWillMount',
+          'componentWillReceiveProps',
+          'shouldComponentUpdate',
+          'componentWillUpdate',
+          'componentDidMount',
+          'componentDidUpdate',
+          'componentWillUnmount',
+          'componentDidCatch'
+        ]
+      }
+    ];
+
+    expect(questions).toEqual(expectedVal);
+  });
+});
